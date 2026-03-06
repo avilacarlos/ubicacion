0a838208316e43c2882c4cfd8b34acd40a67313e
avilacarlos <artnprint01@gmail.com>
2026-03-05 18:16:32 -0600

    eliminacion lottie

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import Parallax from 'parallax-js';
-import '@lottiefiles/lottie-player';
+
 
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -10,21 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
         new Parallax(scene, {
             relativeInput: true, // Importante para que funcione bien con el giroscopio del cel
             hoverOnly: false 
-        });
-    }
-
-    // Animación Lottie
-    const contenedorAnimacion = document.getElementById('contenedor-animacion');
-    const lottiePlayer = document.getElementById('mi-animacion');
-
-    if (contenedorAnimacion && lottiePlayer) {
-        setTimeout(() => {
-            contenedorAnimacion.classList.add('visible');
-            lottiePlayer.play();
-        }, 2000);
-
-        lottiePlayer.addEventListener('complete', () => {
-            contenedorAnimacion.classList.remove('visible');
         });
     }
 });
